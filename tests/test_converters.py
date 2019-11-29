6e827e652cf6d0e23cda8b35f66b1bd88b20f3a1
Diogo Baeder de Paula Pinto <diogo.baeder@yougov.com>
2019-11-29 05:15:32 -0300

    Now experimenting with nested dataclass instances.

--- tests/test_converters.py
+++ tests/test_converters.py
@@ -4,15 +4,33 @@ from kustomize.converters import to_dict
 
 
 @dataclass
-class SomeBaseManifest:
+class SomeSimpleManifest:
     apiVersion: str = 'apps/v1'
     metadata: dict = field(default_factory=dict)
     spec: dict = field(default_factory=dict)
 
 
+@dataclass
+class Metadata:
+    name: str
+
+
+@dataclass
+class Spec:
+    replicas: int = 1
+
+
+@dataclass
+class SomeComplexManifest:
+    kind: str
+    metadata: Metadata
+    spec: Spec
+    apiVersion: str = 'apps/v1'
+
+
 class TestDictConverter:
-    def test_converts_manifest_dataclass_to_dict(self):
-        manifest = SomeBaseManifest(
+    def test_converts_simple_manifest_dataclass_to_dict(self):
+        manifest = SomeSimpleManifest(
             metadata={'foo': 'bar'},
             spec={'foo': 'baz'},
         )
@@ -23,4 +41,20 @@ class TestDictConverter:
             'apiVersion': 'apps/v1',
             'metadata': {'foo': 'bar'},
             'spec': {'foo': 'baz'},
+        }
+
+    def test_converts_complex_manifest_dataclass_to_dict(self):
+        manifest = SomeComplexManifest(
+            kind='Deployment',
+            metadata=Metadata(name='something'),
+            spec=Spec(),
+        )
+
+        result = to_dict(manifest)
+
+        assert result == {
+            'kind': 'Deployment',
+            'apiVersion': 'apps/v1',
+            'metadata': {'name': 'something'},
+            'spec': {'replicas': 1},
         }
